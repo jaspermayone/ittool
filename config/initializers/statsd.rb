3427129625fa20197f85fe67e5b603e5ce3c7dd8
Jasper Mayone <me@jaspermayone.com>
2024-07-01 13:34:17 -0400

    more deploy fixing

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # StatsD config here
   ENV["STATSD_ENV"] = "production" # This won't send data unless set to production
-  ENV["STATSD_ADDR"] = Rails.application.credentials.telem[:endpoint]
+  ENV["STATSD_ADDR"] = Rails.application.credentials.dig(:telem, :endpoint)
   ENV["STATSD_PREFIX"] = "#{Rails.env}.ittool"
 
   StatsD::Instrument::Environment.setup
