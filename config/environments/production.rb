55f943a291e83b8ed6449470a010c6d1f3ae49a1
Jasper Mayone <me@jaspermayone.com>
2024-07-01 13:32:32 -0400

    deploy hotfixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.postmark[:api_token]
-  }
+    api_token: Rails.application.credentials.dig(:postmark, :api_token)
+    }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
